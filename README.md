ede5933841ba34d57832e29b98c2bff32b9c014c
Steven Prichard <spprichard20@gmail.com>
2019-11-10 16:24:56 -0600

    updated readme, just the begining

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # K8s
 
 A description of this package.
+
+## Hitting REST-API for Local Cluster
+- Get Token with
+    - Copy this token, it becomes the bearer token in all future requests
+```
+ kubectl get secrets \
+    -o jsonpath='{.items[?(@.type=="kubernetes.io/service-account-token")].data.token}' \
+    | base64 --decode
+```
+- Get the URL of the cluster you are trying to hit
+    - Any one of these URLS should work
+```
+kubectl config view -o \
+    jsonpath='{.clusters[*].cluster.server}'
+```
+
+## Resources
+- https://kubernetes.io/docs/tasks/administer-cluster/access-cluster-api/
--- Sources/K8s/K8s.swift
+++ Sources/K8s/K8s.swift
@@ -1,3 +1,29 @@
+import Foundation
+
+enum Errors: Error {
+    case CredentialLoadErrorFileLoadError(String)
+}
+
+
+struct K8sDefaultCredentials: Codable {
+    public let clientID: String
+    public let clientSecret: String
+    public let refreshToken: String
+    public let type: String
+    
+    
+    public init(fromFilePath path: String) throws {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        if let contents = try String(contentsOfFile: path).data(using: .utf8) {
+            self = try decoder.decode(K8sDefaultCredentials.self, from: contents)
+        } else {
+            throw Errors.CredentialLoadErrorFileLoadError(path)
+        }
+    }
+}
+
+
 struct K8s {
     var text = "Hello, World!"
 }
