6a231178cc7e29773c19f48b866774df157d9bd0
Steven Prichard <spprichard20@gmail.com>
2019-11-11 00:45:18 -0600

    Kubernetes Proto Files
    - Added command to Makefile for generating files
    - Added command to move files into project
    - Added all the generated file to project
    - NEXT: Add Swifty Wrappers around needed generated code
    - NEXT: Verify this is will work

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # K8s
 
-A description of this package.
+A Pure-Swift Kubernetes Client
+
+## Make Commands
+- `make import`
+    - Copies over proto files located in `k8s.io` into `protos` file so that they can be brought into project
+- `make sync-protos`
+    - Copies proto file from protos directory and places them in the `Sources/K8s` directory so that they can be buillt with the project
+    _NOTE: you may need to add this file into Xcode_
 
 ## Hitting REST-API for Local Cluster
 - Get Token with
--- Sources/K8s/K8s.swift
+++ Sources/K8s/K8s.swift
@@ -1,29 +1,7 @@
 import Foundation
 
-enum Errors: Error {
-    case CredentialLoadErrorFileLoadError(String)
-}
-
-
-struct K8sDefaultCredentials: Codable {
-    public let clientID: String
-    public let clientSecret: String
-    public let refreshToken: String
-    public let type: String
-    
-    
-    public init(fromFilePath path: String) throws {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        if let contents = try String(contentsOfFile: path).data(using: .utf8) {
-            self = try decoder.decode(K8sDefaultCredentials.self, from: contents)
-        } else {
-            throw Errors.CredentialLoadErrorFileLoadError(path)
-        }
-    }
-}
-
-
 struct K8s {
     var text = "Hello, World!"
+    var Job: K8s_Io_Api_Batch_V1_Job
+    
 }
